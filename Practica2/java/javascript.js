3b7af59681b0883e4b1ecc12e57788993bf9e5bd
Mapachana <anabuenrua99@gmail.com>
2021-03-21 18:12:17 +0100

    cambios menores de estilo

--- Practica2/java/javascript.js
+++ Practica2/java/javascript.js
@@ -37,20 +37,15 @@ function submit_comentario(){
 document.getElementById("enviar-comentario").onclick = submit_comentario;
 
 /* Funciones del diálogo modal */
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById("myModal").style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == document.getElementById("myModal")) {
+    document.getElementById("myModal").style.display = "none";
   }
 }
