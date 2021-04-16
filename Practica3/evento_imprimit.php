c2f08b7ef83181a5040dd4d36197c3bc9103a939
Mapachana <anabuenrua99@gmail.com>
2021-04-16 17:01:06 +0200

    pasada pagina al servidor

---
+++ Practica3/evento_imprimit.php
@@ -0,0 +1,20 @@
+<?php
+    require_once "/usr/local/lib/php/vendor/autoload.php";
+
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
+
+    $nombreEvento = "Nombre por defecto";
+    $fechaEvento = "Fecha por defecto"; 
+
+    if($_GET['ev'] == 1){
+        $nombreEvento = "Nombre 1";
+        $fechaEvento = "Fecha 1";
+    }
+    else{
+        $nombreEvento = "Nombre 2";
+        $fechaEvento = "Fecha 2";
+    }
+
+    echo $twig->render('evento_imprimir.html', ['nombre' => $nombreEvento, 'fecha' => $fechaEvento]);
+?>
---
+++ Practica3/templates/java/javascript.js
@@ -0,0 +1,85 @@
+/* Función para mostrar y esconder la barra de comentarios en un evento*/
+function ver_comentarios(){
+    var barra = document.getElementById("apartado-comentarios");
+    barra.classList.toggle("barra-comentarios-activa");
+}
+
+document.getElementById("boton-comentarios").onclick = ver_comentarios;
+
+/* Función para enviar un comentario */
+
+function submit_comentario(){
+    var nombre = document.getElementById("nombre");
+    var email = document.getElementById("email");
+    var texto = document.getElementById("texto-comentario");
+
+    var campos_rellenos = true;
+    
+    var fecha = Date();
+    var regex_correo = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // Expresión regular con la que comparar el correo
+
+    if (nombre.value == ""){
+        document.getElementById("error-modal").innerHTML = "El nombre no debe ser vacío";
+        document.getElementById("myModal").style.display = "block";
+
+        campos_rellenos = false;
+    }
+
+    if (! regex_correo.test(email.value)){
+        document.getElementById("error-modal").innerHTML = "El email no es válido";
+        document.getElementById("myModal").style.display = "block";
+
+        campos_rellenos = false;
+    }
+
+    if (texto.value == ""){
+        document.getElementById("error-modal").innerHTML = "El texto no debe ser vacío";
+        document.getElementById("myModal").style.display = "block";
+
+        campos_rellenos = false;
+    }
+
+    if (campos_rellenos){
+        var m = new Date();
+        var dateString = m.getFullYear() +"/"+ (m.getMonth()+1) +"/"+ m.getDate() + " " + m.getHours() + ":" + m.getMinutes();
+
+        document.getElementById("seccion-comentarios").insertAdjacentHTML("beforeend", "<div class=\"comentario\">" + "<h4>" + nombre.value + " " + dateString + "</h4>" + "<p>" + texto.value + "</p>" + "</div>");
+    }
+}
+
+document.getElementById("enviar-comentario").onclick = submit_comentario;
+
+/* Funcion para corregir palabras */
+
+function reemplazar_palabras(){
+    var palabras = [/caca/g, /uwu/g, /tonto/g, /idiota/g];
+    var palabras_corregidas = ["****", "***", "*****", "******"];
+
+    var texto = document.getElementById("texto-comentario").value;
+
+    var texto_corregido = texto;
+
+    var i;
+    for (i = 0; i < palabras.length; i++){
+        texto_corregido = texto_corregido.replace(palabras[i], palabras_corregidas[i]);
+    }
+
+    document.getElementById("texto-comentario").value = texto_corregido;
+}
+
+document.getElementById("texto-comentario").onchange = reemplazar_palabras;
+document.getElementById("texto-comentario").onkeypress = reemplazar_palabras;
+
+/* Funciones del diálogo modal */
+
+// When the user clicks on <span> (x), close the modal
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == document.getElementById("myModal")) {
+    document.getElementById("myModal").style.display = "none";
+  }
+}
